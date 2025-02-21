69a6d57a11c2cf1359ff76b6a5be07cd3dc4e046
pejj212 <pejj212@gmail.com>
2025-02-21 17:38:24 +0900

    feat : 영화 상세 페이지

--- src/components/Movie/MovieDetailForm.jsx
+++ src/components/Movie/MovieDetailForm.jsx
@@ -1,71 +1,124 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Image, Text, Button, VStack, HStack, Grid, Container } from "@chakra-ui/react";
+import { Box, Text, Button} from "@chakra-ui/react";
+import "../css/Movie/MovieDetailForm.css";
 
 function MovieDetailForm() {
-  const { movieId } = useParams(); //url에서 movieId 뽑아옴
-  const [movie, setMovie] = useState(null); //서버에서 받아온 영화 객체
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
   const [error, setError] = useState(null);
+
+  const viewAgeMapping = {
+    ALL: "전체 이용가",
+    AGE12: "12세 이용가",
+    AGE15: "15세 이용가",
+    AGE19: "19세 이용가",
+  };
+
+  const nationMapping = {
+    KOREA : "한국",
+    USE : "미국",
+    FOREIGN : "해외",
+  };
+
+  const genreMapping = {
+    FANTASY : "판타지",
+    ACTION : "액션",
+    SF : "SF",
+    CRIME : "범죄",
+    COMEDY : "코미디",
+    DRAMA : "드라마",
+    HORROR : "공포",
+    THRILLER : "스릴러",
+    FAMILY : "가족",
+    MYSTERY : "미스터리",
+    ROMANCE : "로맨스",
+    MUSIC : "음악",
+  };
+
+  const ifComingSoon = (date) =>{
+    const release = new Date(date);
+    const today = new Date();
+    return release   > today;
+  }
+
+  const changeDate=(release) => {
+    const data = String(release).replaceAll(",", ".");
+    return data;
+  }
+
+  //이미지 슬라이드 해보기기
+  const imageSlide = () => {
+    const [image, setImage] = useState(0);
+
+
+
+  }
 
   const fetchMovie = async () => {
     try {
       const response = await fetch(`http://localhost:8080/api/movies/${movieId}`);
-
       if (!response.ok) {
         throw new Error("영화를 찾을 수 없습니다.");
       }
-
-      const data = await response.json(); //json으로 변환
-      setMovie(data); //movie 설정정
+      const data = await response.json();
+      setMovie(data);
     } catch (err) {
       setError(err.message);
     }
   };
 
-
-  useEffect(() => { //컴포넌트가 렌더링 될 때마다 실행
-    fetchMovie(); //이 함수를.....
+  useEffect(() => {
+    fetchMovie();
   }, [movieId]);
 
   if (error) return <Text color="red" textAlign="center">{error}</Text>;
   if (!movie) return <Text textAlign="center">로딩 중...</Text>;
 
-  return ( //여기에 html코드 작성하는듯
-    <Container py={5} centerContent> {/*py->상하단 padding*/}
-      <VStack spacing={8} mt={8} width="100%" align="center">
-
-        <HStack align="center" justify="center" spacing={8}>  {/* HStack->가로정렬 VStack->세로정렬 */}
-          <Image
-            src={movie.images?.find(img => img.imageType === "MAIN")?.imagePath || "https://via.placeholder.com/200"} 
+  return (
+    <Box className="container">
+      <div className="movieInfo">
+        <div className="moviePoster">
+          <img
+            src={movie.images?.find(img => img.imageType === "MAIN")?.imagePath}
             alt={movie.title}
-            boxSize="250px"
-            objectFit="contain" //contain으로 해야 이미지를 안 자름
+            className="movie-poster-image"
           />
-          <VStack align="start" > {/* spacing 속성 뭐가 변하는지 모르겠음 */}
-            <Text fontSize="2xl" fontWeight="bold">{movie.title}</Text> {/* 2xl->24px, md->16px */}
-            <Text fontSize="md" color="gray.600">{movie.releaseDate} 개봉 | {movie.genre} | {movie.viewAge} 관람가</Text>
-            <Text><strong>감독:</strong> {movie.director}</Text>
-            <Text><strong>배우:</strong> {movie.actors}</Text>
-            <Text><strong>줄거리??</strong></Text>
-            <Button colorScheme="gray" size="lg">예매하기</Button>
-          </VStack>
-        </HStack>
-        <Grid templateColumns="repeat(3, 1fr)" gap={4} justifyContent="center">
-          {movie.images?.filter(img => img.imageType === "STILL").map(image => (
-            <Image 
-            key={image.imageId} 
-            src={image.imagePath} 
-            alt={image.imageName} 
-            boxSize="250px" 
-            objectFit="contain" 
-            borderRadius="md" />
-          ))}
-        </Grid>
+        </div>
 
+        <div className="movieDetail">
+          <div className="movieDetail_title">
+          <h1 className="title">{movie.title}</h1>
+          </div>
+          <p className="movieMeta">
+            {changeDate(movie.releaseDate)} 개봉 | {movie.runTime}분 |  {viewAgeMapping[movie.viewAge] || "등급 정보 없음"} | {nationMapping[movie.nation]}
+          </p>
+          <p className="metaDetail"><strong>감독 </strong> {movie.director}</p>
+          <p className="metaDetail"><strong>배우 </strong> {movie.actors}</p>
+          <p className="metaDetail"><strong>장르 </strong> {genreMapping[movie.genre1]}, {genreMapping[movie.genre2]}</p>
+          <div className="button">
+          <Button colorPalette={ifComingSoon(movie.releaseDate) ? "gray" : "red"} disabled={ifComingSoon(movie.releaseDate)} size="xl">{ifComingSoon(movie.releaseDate) ? "상영 예정" : "예매하기"}</Button>
+          </div>
+        </div>
+      </div>
 
-      </VStack>
+      <hr className="hr" />
 
-    </Container>
+      <div className="movieSynopsis">
+        <h2 className="synopTitle">상세 정보</h2>
+        <p className="synopsis">{movie.synopsis}</p>
+      </div>
+
+      <div className="movieSynopsis">
+        <br />
+        <br />
+        <div className="stillcut">
+        {movie.images?.filter(img => img.imageType === "STILL").map(image => (
+          <img key={image.imageId} className="gallery-image" src={image.imagePath} alt={image.imageName} />
+        ))}
+      </div>
+      </div>
+    </Box>
   );
 }
 
